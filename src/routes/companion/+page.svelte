b4dded82fb9780d6e8c9805b887b9533c7263e1c
j-scheid <64362990+j-scheid@users.noreply.github.com>
2023-02-08 21:35:16 +0100

    added img generation + fixes

--- src/routes/companion/+page.svelte
+++ src/routes/companion/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
 	import { createClient } from '@supabase/supabase-js';
-	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { access_token } from '$lib/data/stores.js';
+	import { modalStore } from '@skeletonlabs/skeleton';
+	import type { ModalSettings, ModalComponent } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
 
 	let refresh_token: string;
+	const updateFrequency = 3000; //check spotify API every seconds
+	let noSongHintVisible: boolean = true;
+	let generatorCount = 0;
+	let currImg = '';
+
 	access_token.subscribe((value) => {
 		refresh_token = value;
+		if(refresh_token === ''){
+			goto('/login')
+		}
 	});
 
 	const supabase = createClient(
@@ -14,11 +24,12 @@
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlanhkdHR4ZnZwY3pwZWJ1dmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2MDAzMzksImV4cCI6MTk5MTE3NjMzOX0.LJUk1o-yoU9yqCZvoJPYtkaR8o3lzjmRFYFv7JEVJso'
 	);
 	function spotifyLogout() {
-		console.log('Sign out!');
 		signout();
+		goto('/login')
 	}
 	async function signout() {
 		const { error } = await supabase.auth.signOut();
+		access_token.set('')
 	}
 
 	const client_id = 'f9877a5f4b0846888428579265af8e0d';
@@ -47,7 +58,14 @@
 	let data: any;
 	$: songName = '';
 	$: artistName = '';
+	let previousSong: string;
+	let previousArtist: string;
 	onMount(async () => {
+		getNowPlaying()
+		var intervalID = window.setInterval(getNowPlaying, updateFrequency);
+	});
+
+	async function getNowPlaying(): Promise<void> {
 		const { access_token } = await getAccessToken();
 
 		const response = await fetch(NOW_PLAYING_ENDPOINT, {
@@ -55,16 +73,99 @@
 				Authorization: `Bearer ${access_token}`
 			}
 		});
-		data = await response.json();
-		songName = data.item.name;
-		artistName = data.item.artists[0].name;
-	});
+
+		if (response.status === 204) {
+			noSongHintVisible = true;
+		} else {
+			noSongHintVisible = false;
+			data = await response.json();
+			previousSong = songName;
+			previousArtist = artistName;
+			songName = data.item.name;
+			artistName = data.item.artists[0].name;
+			checkSongChange(songName, artistName);
+		}
+	}
+
+	function checkSongChange(song: string, artist: string): void {
+		if (song != previousSong || artist != previousArtist) {
+			if (generatorCount <= 3) {
+				console.log(generatorCount);
+				generatorCount++;
+				generateImage(song);
+			} else {
+				triggerAlert();
+			}
+		}
+	}
+
+	let imgUrl: string;
+	async function generateImage(prompt: string) {
+		const encodedParams = new URLSearchParams();
+		encodedParams.append('prompt', prompt);
+		encodedParams.append('guidance', '8');
+		encodedParams.append('height', '320');
+		encodedParams.append('width', '320');
+		encodedParams.append('steps', '15');
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/x-www-form-urlencoded',
+				'X-RapidAPI-Key': 'd30b87e183msh2179781cc02dde5p18be77jsn483ca2b1534b',
+				'X-RapidAPI-Host': 'dezgo.p.rapidapi.com'
+			},
+			body: encodedParams
+		};
+
+		var response = await fetch('https://dezgo.p.rapidapi.com/text2image', options);
+		console.log(response);
+		var pngBlob = await response.blob();
+
+		console.log('Got the image as a blob:', pngBlob);
+
+		imgUrl = URL.createObjectURL(pngBlob);
+		saveImg(imgUrl)
+	}
+
+	async function saveImg(src: string){
+		const url = src;
+		const blob = await fetch(url).then((r) => r.blob());
+		await supabase.storage.from('generated-img').upload('TestBatch/' + songName + '-' + artistName + '-' + Math.random().toString(36).substr(2, 5) + '.jpeg', blob);
+	}
+
+	function triggerAlert(): void {
+		const alert: ModalSettings = {
+			type: 'alert',
+			title: 'Limit exceeded!',
+			body: 'This is a project funded by students so we kindly ask you not to abuse the image generator. You can refresh the page and log in again to receive additional 3 image generations. Thank you for understanding.',
+			image: '',
+			buttonTextCancel: 'I understand'
+		};
+		modalStore.trigger(alert);
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center">
-		<h2 class="font-bold">{songName}</h2>
-		<h4 class="font-bold">{artistName}</h4>
+	<div class="text-center">
+		{#if noSongHintVisible}
+			<aside class="alert variant-ghost-primary">
+				<div class="alert-message">
+					<h3>No song playing</h3>
+					<p>Feel free to play something on Spotify.</p>
+				</div>
+			</aside>
+		{:else}
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<img
+				src={imgUrl}
+				class="object-center w-80 rounded-lg"
+				alt="We are generating an image for you..."
+			/><br />
+			<h2 class="font-bold">{songName}</h2>
+			<h4 class="font-bold">{artistName}</h4>
+		{/if}
+<br><br><br>
 		<button on:click={spotifyLogout}>Sign Out</button>
 	</div>
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,62 +1,41 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script>
-	import { createClient } from '@supabase/supabase-js'
-	import {page} from '$app/stores'
+	import { createClient } from '@supabase/supabase-js';
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import 'openai'
 
-	const OAIKEY = "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"
+	const OAIKEY = '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';
 
-	const supabase = createClient('https://lejxdttxfvpczpebuvki.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlanhkdHR4ZnZwY3pwZWJ1dmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2MDAzMzksImV4cCI6MTk5MTE3NjMzOX0.LJUk1o-yoU9yqCZvoJPYtkaR8o3lzjmRFYFv7JEVJso')
+	const supabase = createClient(
+		'https://lejxdttxfvpczpebuvki.supabase.co',
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlanhkdHR4ZnZwY3pwZWJ1dmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2MDAzMzksImV4cCI6MTk5MTE3NjMzOX0.LJUk1o-yoU9yqCZvoJPYtkaR8o3lzjmRFYFv7JEVJso'
+	);
 
-	function spotifyLogin(){
-		signInWithSpotify()
+	function spotifyLogin() {
+		signInWithSpotify();
 	}
-	function spotifyLogout(){
-		console.log("Sign out!")
-		signout()
-	}
+
 	async function signInWithSpotify() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'spotify',
-	options: {
-		scopes: 'user-read-currently-playing'
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'spotify',
+			options: {
+				scopes: 'user-read-currently-playing'
+			}
+		});
 	}
-  })
-}
-async function signout() {
-  const { error } = await supabase.auth.signOut()
-}
+
 	onMount(async () => {
-		const clientId = "your-client-id-here";  // Replace with your client id
-const params = new URLSearchParams(window.location.hash.substring(1));
-const code = params.get("access_token");
-console.log(code)
-	});
+		const params = new URLSearchParams(window.location.hash.substring(1));
+		const code = params.get('access_token');
+		console.log(code);
 
-
-/* const { Configuration, OpenAIApi } = require("openai");
-
-async function generateImg(){
-	const configuration = new Configuration({
-  apiKey: OAIKEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.createImage({
-  prompt: "A cute baby sea otter",
-  n: 2,
-  size: "1024x1024",
-});
-console.log(response)
-} */
-
-
-
+	})
 </script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center">
 		<h2 class="font-bold">Welcome.</h2>
-		<button on:click={spotifyLogin}>Log In with Spotify</button><br>
-		<button on:click={spotifyLogout}>Sign Out</button>
+		<p>You can connect your Spotify account by loggin in through the button below. We will then create an image for the currently playing songs.</p>
+		<button on:click={spotifyLogin} class="btn variant-filled-primary lg">Log In with Spotify</button><br />
 	</div>
 </div>
